95dedf03ddc76fdddf352431a52717466b0133fe
Tom Vacherat <tvacherat@gmail.com>
2020-12-24 14:18:44 +0100

    LIBFT v3

---
+++ srcs/includes/libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvachera <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/16 16:20:44 by tvachera          #+#    #+#             */
+/*   Updated: 2020/12/24 12:05:38 by tvachera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdbool.h>
+# include "headers/chain_list.h"
+# include "headers/display.h"
+# include "headers/ft_printf.h"
+# include "headers/ft_string.h"
+# include "headers/get_next_line.h"
+# include "headers/memory.h"
+
+#endif
